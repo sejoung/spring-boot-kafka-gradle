ca7f5dad0ca51101e98224982f2dbbdc8c72f3a3
sejoung kim <sejoung@gmail.com>
2021-06-03 16:41:18 +0900

    se

--- src/main/java/io/github/sejoung/kafka/consumer/OrderPayedEventConsumer.java
+++ src/main/java/io/github/sejoung/kafka/consumer/OrderPayedEventConsumer.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderPayedEventConsumer {
 
 	@KafkaListener(topics = "${topic.event}", groupId = "list")
-	public void receive(List<ConsumerRecord<?, ?>> records) {
+	public void receive(List<ConsumerRecord<?, ?>> records, Acknowledgment ack) {
 		log.info("records size='{}'", records.size());
 		records.forEach(consumerRecord -> System.out.println(consumerRecord.value()));
+		ack.acknowledge();
 	}
 }
